b7bb5c468b30d057d8cd64a5ed5813e2ca25a621
hyogman <henryyogman@gmail.com>
2018-02-18 19:10:29 +0100

    use graph cool and add some fe libs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./components/test";
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Test />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ client/src/components/test.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { Button } from "antd";
+
+export default function Test() {
+  return <Button type="primary">Primary</Button>;
+}
